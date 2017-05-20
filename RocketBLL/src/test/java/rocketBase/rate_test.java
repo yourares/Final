cd9f6590578b4535911d2b293aa44a11ee469b4f
appleud <appleud@192.168.0.108>
2017-05-19 20:29:31 -0400

    first

---
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -0,0 +1,18 @@
+package rocketBase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import exceptions.RateException;
+
+public class rate_test {
+
+	//TODO - RocketBLL rate_test
+	//		Check to see if a known credit score returns a known interest rate
+	
+	//TODO - RocketBLL rate_test
+	//		Check to see if a RateException is thrown if there are no rates for a given
+	//		credit score
+
+}
