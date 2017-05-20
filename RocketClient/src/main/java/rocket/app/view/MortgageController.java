14ed3236e68a9105c07107f4a1c28a14b040f6b5
appleud <appleud@192.168.0.108>
2017-05-19 20:37:29 -0400

    first

---
+++ RocketClient/src/main/java/rocket/app/view/MortgageController.java
@@ -0,0 +1,52 @@
+package rocket.app.view;
+
+import eNums.eAction;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import rocket.app.MainApp;
+import rocketCode.Action;
+import rocketData.LoanRequest;
+
+public class MortgageController {
+
+	@FXML TextField txtCreditScore;
+	@FXML TextField txtMortgageAmt;
+	
+	private TextField txtNew;
+	
+	private MainApp mainApp;
+	
+
+	public void setMainApp(MainApp mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	
+	@FXML
+	public void btnCalculatePayment(ActionEvent event)
+	{
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Message Here...");
+		alert.setHeaderText("Look, an Information Dialog");
+		alert.setContentText(txtNew.getText());
+		alert.showAndWait().ifPresent(rs -> {
+		    if (rs == ButtonType.OK) {
+		        System.out.println("Pressed OK.");
+		    }
+		});
+	}
+	
+	public void HandleLoanRequestDetails(LoanRequest lRequest)
+	{
+		//	TODO - RocketClient.HandleLoanRequestDetails
+		//			lRequest is an instance of LoanRequest.
+		//			after it's returned back from the server, the payment (dPayment)
+		//			should be calculated.
+		//			Display dPayment on the form, rounded to two decimal places
+		
+	}
+}
