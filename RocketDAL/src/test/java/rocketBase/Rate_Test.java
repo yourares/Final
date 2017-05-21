8b2d4ca17d5696848de57942d140c4fbc9725b9c
appleud <appleud@192.168.0.108>
2017-05-21 15:38:47 -0400

    change

--- RocketDAL/src/test/java/rocketBase/Rate_Test.java
+++ RocketDAL/src/test/java/rocketBase/Rate_Test.java
@@ -10,20 +10,10 @@ import rocketDomain.RateDomainModel;
 
 public class Rate_Test {
 
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a known credit score returns a known interest rate
-	
-	//TODO - RocketDAL rate_test
-	//		Check to see if a RateException is thrown if there are no rates for a given
-	//		credit score
-
-@Test
-public void RateTest(){
-	ArrayList<RateDomainModel> Rates = RateDAL.getAllRates();
-	System.out.println ("Rates Size: " + Rates.size());
-	assertTrue(Rates.size() > 0);
-	
-	
-}
+	@Test
+	public void RateTest() {
+		ArrayList<RateDomainModel> Rates = RateDAL.getAllRates();
+		System.out.println("Rates Size: " + Rates.size());
+		assertTrue(Rates.size() > 0);
+	}
 }
