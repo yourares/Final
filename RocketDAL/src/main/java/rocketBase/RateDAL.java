62a91b022675997a5dd02e6f77dd5e86f526fcbb
appleud <appleud@192.168.0.108>
2017-05-20 22:57:39 -0400

    change

--- RocketDAL/src/main/java/rocketBase/RateDAL.java
+++ RocketDAL/src/main/java/rocketBase/RateDAL.java
@@ -25,11 +25,6 @@ public class RateDAL {
 		try {
 			tx = session.beginTransaction();	
 			
-			//TODO - RocketDALRateDAL.getAllRates
-			//			probably not a bad idea to sort the results...  Add an OrderBy
-			//			example can be found here:
-			//  		http://www.tutorialspoint.com/hibernate/hibernate_query_language.htm			
-			//List lstRates = session.createQuery("FROM RateDomainModel r Order By r.iMinCreditScore").list();
 			List lstRates = session.createQuery("FROM RateDomainModel").list();
 
 			for (Iterator iterator = lstRates.iterator(); iterator.hasNext();) {
