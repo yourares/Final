337736d20a74780d6b9288b245e110b6046930f8
appleud <appleud@192.168.0.108>
2017-05-21 00:26:42 -0400

    change

--- RocketServer/src/main/java/rocketServer/RocketHub.java
+++ RocketServer/src/main/java/rocketServer/RocketHub.java
@@ -1,16 +1,15 @@
 package rocketServer;
 
 import java.io.IOException;
-
+import exceptions.RateException;
 import netgame.common.Hub;
 import rocketBase.RateBLL;
 import rocketData.LoanRequest;
 
-
 public class RocketHub extends Hub {
 
 	private RateBLL _RateBLL = new RateBLL();
-	
+
 	public RocketHub(int port) throws IOException {
 		super(port);
 	}
@@ -18,23 +17,19 @@ public class RocketHub extends Hub {
 	@Override
 	protected void messageReceived(int ClientID, Object message) {
 		System.out.println("Message Received by Hub");
-		
+
 		if (message instanceof LoanRequest) {
 			resetOutput();
-			
-			LoanRequest lq = (LoanRequest) message;
-			
-			//	TODO - RocketHub.messageReceived
 
-			//	You will have to:
-			//	Determine the rate with the given credit score (call RateBLL.getRate)
-			//		If exception, show error message, stop processing
-			//		If no exception, continue
-			//	Determine if payment, call RateBLL.getPayment
-			//	
-			//	you should update lq, and then send lq back to the caller(s)
-			
-			sendToAll(lq);
+			LoanRequest lq = (LoanRequest) message;
+			try {
+				lq.setdRate(RateBLL.getRate(lq.getiCreditScore()));
+				lq.setdPayment(RateBLL.getPayment(lq.getdRate()/1200, lq.getiTerm(), lq.getdAmount(), lq.getdPayment(), false));
+				sendToAll(lq);
+			} catch (RateException e) {
+				sendToAll(e);
+
+			}
 		}
 	}
 }
