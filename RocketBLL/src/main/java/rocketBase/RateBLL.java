0dbff216929bbd1aceb79fe984107969dfe29303
appleud <appleud@192.168.0.108>
2017-05-20 23:48:04 -0400

    change

--- RocketBLL/src/main/java/rocketBase/RateBLL.java
+++ RocketBLL/src/main/java/rocketBase/RateBLL.java
@@ -14,20 +14,21 @@ public class RateBLL {
 	static double getRate(int GivenCreditScore) throws RateException 
 	{
 		double dInterestRate = 0;
-		
+		ArrayList<RateDomainModel> Rates = RateDAL.getAllRates();
 		//TODO - RocketBLL RateBLL.getRate - make sure you throw any exception
-		
+		return dInterestRate;
+
 		//		Call RateDAL.getAllRates... this returns an array of rates
 		//		write the code that will search the rates to determine the 
 		//		interest rate for the given credit score
 		//		hints:  you have to sort the rates...  you can do this by using
 		//			a comparator... or by using an OrderBy statement in the HQL
 		
-		
+	}		
 		//TODO - RocketBLL RateBLL.getRate
 		//			obviously this should be changed to return the determined rate
 		
-		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
+
 		
 
 		//TODO: Filter the ArrayList...  look for the correct rate for the given credit score.
@@ -36,20 +37,7 @@ public class RateBLL {
 		//	Example... how to use Lambda functions:
 		//			https://github.com/CISC181/Lambda
 		
-		return dInterestRate;
-		
-		
-	}
-	
-	
 
-	
-	
-	
-	
-	//TODO - RocketBLL RateBLL.getPayment 
-	//		how to use:
-	//		https://poi.apache.org/apidocs/org/apache/poi/ss/formula/functions/FinanceLib.html
 	
 	public static double getPayment(double r, double n, double p, double f, boolean t)
 	{		
