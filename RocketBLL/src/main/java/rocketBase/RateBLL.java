5dc840856ebc165ebd80c6d664b26bb06c167bdc
appleud <appleud@192.168.0.108>
2017-05-21 15:31:54 -0400

    change

--- RocketBLL/src/main/java/rocketBase/RateBLL.java
+++ RocketBLL/src/main/java/rocketBase/RateBLL.java
@@ -5,40 +5,29 @@ import java.util.ArrayList;
 import org.apache.poi.ss.formula.functions.*;
 
 import exceptions.RateException;
+import rocketData.LoanRequest;
 import rocketDomain.RateDomainModel;
 
 public class RateBLL {
 
 	private static RateDAL _RateDAL = new RateDAL();
 	
-	static double getRate(int GivenCreditScore) throws RateException 
+	public static double getRate(int GivenCreditScore) throws RateException 
 	{
 		double dInterestRate = 0;
-		ArrayList<RateDomainModel> Rates = RateDAL.getAllRates();
-		//TODO - RocketBLL RateBLL.getRate - make sure you throw any exception
+		ArrayList<RateDomainModel> AllRates = RateDAL.getAllRates();
+		
+		for(int i = 0;i < AllRates.size();i++){
+			if(AllRates.get(i).getiMinCreditScore()<GivenCreditScore){
+				dInterestRate = AllRates.get(i).getdInterestRate();
+			}
+			else {
+				throw new RateException(null);
+			}
+		}
 		return dInterestRate;
-
-		//		Call RateDAL.getAllRates... this returns an array of rates
-		//		write the code that will search the rates to determine the 
-		//		interest rate for the given credit score
-		//		hints:  you have to sort the rates...  you can do this by using
-		//			a comparator... or by using an OrderBy statement in the HQL
+	}				
 		
-	}		
-		//TODO - RocketBLL RateBLL.getRate
-		//			obviously this should be changed to return the determined rate
-		
-
-		
-
-		//TODO: Filter the ArrayList...  look for the correct rate for the given credit score.
-		//	Easiest way is to apply a filter using a Lambda function.
-		//
-		//	Example... how to use Lambda functions:
-		//			https://github.com/CISC181/Lambda
-		
-
-	
 	public static double getPayment(double r, double n, double p, double f, boolean t)
 	{		
 		return FinanceLib.pmt(r, n, p, f, t);
